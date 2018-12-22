714eb7fbe6923b141c7ec4a21263c72092ce1d7a
Ruslan Shashkov <ruslanchek@icloud.com>
2018-12-22 10:38:53 +0200

    SW

--- src/app/ts/app.ts
+++ src/app/ts/app.ts
@@ -4,3 +4,19 @@ import * as ReactDOM from 'react-dom';
 import { App } from './components/App';
 
 ReactDOM.render(React.createElement(App), document.getElementById('app'));
+
+if (navigator.serviceWorker) {
+	if (navigator.serviceWorker.controller) {
+		console.log('Active service worker found, no need to register');
+	} else {
+		navigator.serviceWorker
+			.register('/pwabuilder-sw.js', {
+				scope: './',
+			})
+			.then(reg => {
+				console.log(
+					'Service worker has been registered for scope:' + reg.scope,
+				);
+			});
+	}
+}
