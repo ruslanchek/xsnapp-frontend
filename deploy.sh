4148940d9e660f2a1696b297a35c545bf4a19f9d
Ruslan Shashkov <ruslanchek@icloud.com>
2019-01-10 22:58:49 +0200

    Upd

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+
+PORT=22
+USERNAME=xsnapp
+ADDRESS=xsnapp.com
+APP_DIR=/home/${USERNAME}/apps/frontend
+
+yarn install
+yarn build:prod
+ssh -p ${PORT} -l ${USERNAME} ${ADDRESS} "mkdir ${APP_DIR} -p"
+rsync -av ./dist ${USERNAME}@${HOSTNAME}:${APP_DIR}
