e88f479339cb88e5c1596fc9de04632893b2c3a5
Ruslan Shashkov <ruslanchek@icloud.com>
2019-01-15 00:51:12 +0200

    sad

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,8 +3,8 @@ const packageData = require('./package');
 const HandlebarsPlugin = require('handlebars-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 let plugins = [
 	new CleanWebpackPlugin(['dist'], {
@@ -44,7 +44,23 @@ module.exports = {
 	},
 
 	optimization: {
-		minimizer: [new TerserPlugin()],
+		minimize: true,
+		minimizer: [
+			new UglifyJsPlugin({
+				sourceMap: false,
+				uglifyOptions: {
+					safari10: true,
+					compress: {
+						dead_code: true,
+						drop_console: true,
+					},
+					output: {
+						comments: false,
+						beautify: false,
+					},
+				},
+			}),
+		],
 		splitChunks: {
 			chunks: 'all'
 		}
